7a1796363f5986812f722d710a6a1a0b8cfdf070
João Victor Michaeli <161324070+JoaoMichaeli@users.noreply.github.com>
2025-04-22 20:58:49 -0300

    Add project files.

---
+++ Fiap.Estrutura.Controller/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiap.Estrutura.Repository;
+
+
+namespace Fiap.Estrutura.Controller
+{
+    public class ClienteController
+    {
+        public bool validarUsuario(string usuario, string senha)
+        {
+            Cliente
+        }
+    }
+}
---
+++ Fiap.Estrutura.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Fiap.Estrutura.Model;
+using Fiap.Estrutura.Controller;
+
+UsuarioController controler = new UsuarioController();
+
+controler.validarUsuarioSenha("sabrina", "123@");
+
+
+
